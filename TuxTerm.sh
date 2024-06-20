3241daee2c6d87c4b9e4d65986f8219efb581f13
Aeternus <longnecklion126@gmail.com>
2024-06-20 15:20:59 -0700

    Update TuxTerm.sh

--- TuxTerm.sh
+++ TuxTerm.sh
@@ -2,7 +2,7 @@
 # Authors of TuxTerm: Aeternusdio.
 # The MIT License (MIT)
 #
-# Copyright (c) 2024 Apache Software Production. (ASP)
+# Copyright (c) 2024 QUAKTECH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@
 # SOFTWARE.
 declare -A Versions=(
     [tuxterm]="Version 0.3.0"
-    [asp-licence]="Version 2.0.0"
+    [quaktech license]="Version 2.0.0"
 )
 
 # Sources
